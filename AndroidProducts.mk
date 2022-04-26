948acd44065ff1a749b6d504a449b0cfc776ae8e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-26 11:53:56 +0200

    AOP_sprout: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: hmd Model: Nokia 9 Script version: 2.2.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_AOP_sprout.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_AOP_sprout-user \
+    omni_AOP_sprout-userdebug \
+    omni_AOP_sprout-eng
---
+++ omni_AOP_sprout.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from AOP_sprout device
+$(call inherit-product, device/hmd/AOP_sprout/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := AOP_sprout
+PRODUCT_NAME := omni_AOP_sprout
+PRODUCT_BRAND := Nokia
+PRODUCT_MODEL := Nokia 9
+PRODUCT_MANUFACTURER := hmd
